26d018c299d28a6822d874b184f72fb798194a3b
Iain Taylor <iain.taylor.94@gmail.com>
2023-11-20 13:53:53 +0000

    completed readHand()

--- .ccls-cache/@home@runner@C22PokerHands/main.c
+++ .ccls-cache/@home@runner@C22PokerHands/main.c
@@ -21,6 +21,12 @@ FILE *gInputFile;
 
 // Declare Functions
 void usage (char *);
+
+// Read Hand
+enum value getValue (char);
+enum suit getSuit (char);
+struct hand readHand (void);
+
 // Rank Functions - straight/flush/straight flush
 int straight (struct hand);
 bool flush (struct hand);
@@ -60,7 +66,89 @@ void usage (char *cmd) {
   exit (EXIT_SUCCESS);
 }
 
+enum value getValue (char c) {
+  switch (c) {
+    case '2':
+      return TWO;
+      break;
+    case '3':
+      return THREE;
+      break;
+    case '4':
+      return FOUR;
+      break;
+    case '5':
+      return FIVE;
+      break;
+    case '6':
+      return SIX;
+      break;
+    case '7':
+      return SEVEN;
+      break;
+    case '8':
+      return EIGHT;
+      break;
+    case '9':
+      return NINE;
+      break;
+    case 'J':
+      return JACK;
+      break;
+    case 'Q':
+      return QUEEN;
+      break;
+    case 'K':
+      return KING;
+      break;
+    case 'A':
+      return ACE;
+      break;
+    default:
+      printf ("Invalid Value: %c\n", c);
+      return -1;
+      break;
+  }  
+}
 
+enum suit getSuit (char c) {
+  switch (c) {
+    case 'C':
+      return CLUBS;
+      break;
+    case 'D':
+      return DIAMONDS;
+      break;
+    case 'H':
+      return HEARTS;
+      break;
+    case 'S':
+      return SPADES;
+      break;
+    default:
+      printf ("Invalid Suit: %c\n", c);
+      return -1;
+      break;
+  }
+}
+
+struct hand readHand (void) {
+  // I/O
+  const int lineLength = 31;
+  char inputArray[lineLength];
+  fgets (inputArray, lineLength, gInputFile);
+  inputArray[lineLength - 1] = '\0'; // Replace '\n' with '\0'
+
+  // Fill Hand
+  struct hand h;
+  for (int i = 0; i < gCardsInHand; i++) {
+    h.c[i].v = getValue(inputArray[i * 3 + 0]); // Get Value
+    h.c[i].s = getSuit(inputArray[i * 3 + 1]); // Get Suit
+    // Strip Newline - do nothing
+  }
+
+  return h;
+}
 
 
 // Rank Functions - straight/flush/straight-flush
@@ -111,5 +199,6 @@ int straightFlush (struct hand h) {
 
   return -1;
 }
+
 
 
